78ae0431edda796e2055eb0f14a138017f7a486a
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-12-02 17:07:21 +0300

    bookproject01. lesson_01 Дополнение и окончания новой страницы

--- bookproject01/lesson_01/index.php
+++ bookproject01/lesson_01/index.php
@@ -19,19 +19,31 @@ $value1 = 100;
 $value2 = 2.5;
 
 $value3 = 45 + 15;
-echo $value3;
+echo "$value3<br>";
 
 $result1 = $value1 * $value2 - $value3;
-echo $result1;
+echo "$result1<br>";
 
 $result2 = 7 / 3;
-echo $result2;
+echo "$result2<br>";
 
 // слишком длинное, на наш взгляд... Давайте сократим его до второго знака после запятой, для чего вызовем
 // функцию round. Тут же видим, что переменная $result2 переписывается с новями параметрами
 $result2 = round($result2, 2);
+echo "$result2<br>";
 
+// Строки
+$str1 = 'Прораммная платформа ';
 
+// оператор . (точка) выполняет объединение (конкатенацию) строк
+$str2 = $str1.'PHP!';
+
+echo "$str2<br>";
+
+// Вывод данных в PHP
+$s1 = 'PHP';
+$s2 = 'MySQL';
+echo  $s1, ' ', $s2;
 
 ?>
 
